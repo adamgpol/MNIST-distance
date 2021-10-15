dc6a9d425e881560187f331027ca22a713ef73d7
adamgpol <adamgpol@gmail.com>
2021-10-15 11:35:59 +0100

    s

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from tensorflow.keras.datasets import mnist
-from src.distances import train_autoencoder
+from src.distances import train_autoencoder,class_distances
 import numpy as np
 from matplotlib import pyplot as plt
 from IPython import display # If using IPython, Colab or Jupyter
@@ -12,22 +12,25 @@ LATENT_SIZE = 10
 save_file='./autoencoder_models/latent_size_10'
 
 encoder, model= train_autoencoder(x_train,y_train,
-                                save_file=save_file,
+                                save_file=None,
                                 load_file=save_file,
-                                epochs=10,
+                                epochs=0,
                                 batch_size=128)
 
-fig, axs = plt.subplots(4, 8)
-rand = x_test[np.random.randint(0, 10000, 16)].reshape((4, 4, 1, 28, 28))
+# fig, axs = plt.subplots(4, 8)
+# rand = x_test[np.random.randint(0, 10000, 16)].reshape((4, 4, 1, 28, 28))
+#
+# display.clear_output() # If you imported display from IPython
+#
+# for i in range(4):
+#     for j in range(4):
+#         axs[i, j].imshow(model(rand[i, j])[0], cmap = "gray")
+#         axs[i, j].axis("off")
+#         axs[i, j+4].imshow((rand[i, j])[0], cmap = "gray")
+#         axs[i, j+4].axis("off")
+#
+# plt.subplots_adjust(wspace = 0, hspace = 0)
+# plt.show()
+print(y_train.top())
 
-display.clear_output() # If you imported display from IPython
-
-for i in range(4):
-    for j in range(4):
-        axs[i, j].imshow(model(rand[i, j])[0], cmap = "gray")
-        axs[i, j].axis("off")
-        axs[i, j+4].imshow((rand[i, j])[0], cmap = "gray")
-        axs[i, j+4].axis("off")
-
-plt.subplots_adjust(wspace = 0, hspace = 0)
-plt.show()
+distances=class_distances(10,x_train,y_train,encoder)
